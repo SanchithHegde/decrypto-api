31a9c6d196a372dbc33f589e7c9317b658cfdc72
Sanchith Hegde <sanchith.hegde01@gmail.com>
2021-07-25 18:09:30 +0530

    schemas(__init__): Remove unused schema imports
    Signed-off-by: Sanchith Hegde <sanchith.hegde01@gmail.com>

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -4,12 +4,10 @@ Pydantic schemas.
 
 from app.schemas.message import Message
 from app.schemas.question import (
-    Question,
     QuestionAnswer,
     QuestionCreate,
-    QuestionInDB,
     QuestionListItem,
     QuestionUpdate,
 )
 from app.schemas.token import Token, TokenPayload
-from app.schemas.user import User, UserCreate, UserInDB, UserUpdate
+from app.schemas.user import User, UserCreate, UserUpdate
