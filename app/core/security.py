38ef52b7a442aadabcc0d783b8a83e45380f8b29
Sanchith Hegde <sanchith.hegde01@gmail.com>
2021-08-01 13:42:22 +0530

    core(security): Use argon2 instead of bcrypt for password hashing
    Signed-off-by: Sanchith Hegde <sanchith.hegde01@gmail.com>

--- app/core/security.py
+++ app/core/security.py
@@ -10,7 +10,7 @@ from passlib.context import CryptContext  # type: ignore
 
 from app.core.config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 
 ALGORITHM = "HS256"
