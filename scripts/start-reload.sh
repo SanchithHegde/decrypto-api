16df957f3a528ac3bde8deedb16b95b2ff918141
Sanchith Hegde <sanchith.hegde01@gmail.com>
2021-11-22 14:11:43 +0530

    scripts: Use bash interpreter for shell scripts
    Signed-off-by: Sanchith Hegde <sanchith.hegde01@gmail.com>

--- scripts/start-reload.sh
+++ scripts/start-reload.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
 set -euo pipefail
 
@@ -11,7 +11,7 @@ LOG_LEVEL="info"
 # Run prestart.sh script in the "scripts" directory before starting
 PRE_START_PATH="scripts/prestart.sh"
 echo "Running pre-start script ${PRE_START_PATH}"
-sh "${PRE_START_PATH}"
+bash "${PRE_START_PATH}"
 
 # Start Uvicorn with live reload
 exec uvicorn --reload --reload-dir "${APP_DIR}" --host "${HOST}" --port "${PORT}" --log-level "${LOG_LEVEL}" "${APP_MODULE}"
